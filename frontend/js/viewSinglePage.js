35400bf3672f303b6326328f90d067ec6e96ee7a
Obiora Agbasiere <obi.mexy@gmail.com>
2019-02-07 16:50:19 +0100

    users can view all political parties

--- frontend/js/viewSinglePage.js
+++ frontend/js/viewSinglePage.js
@@ -3,16 +3,16 @@ const getParamaterId = () => {
 }
 
 const getParty = (callback) => {
-    return fetch(`${apiNavigations('party')}/${getParamaterId()}`, {
+    return fetch(`${apiNavigations('party')}/${getParamaterId() || '' }`, {
     headers: {
     'x-access-token': localStorage.getItem('token')
     }})
         .then((response) => {
             return response.json().then((resp) => {
-                if (response.status === 404) {
+                if (response.status === 404 || response.status === 401 ) {
                     return callback(resp.error, null);
                 }
-                return callback(null, resp.data[0][0]);
+                return callback(null, resp.data[0]);
             })
         })
 }

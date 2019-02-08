679a03c6b2ea77126424df0304020d0574034207
Emmanuel Daniel <emmsdan@users.noreply.github.com>
2019-02-08 12:58:39 +0100

    Update office.js

--- frontend/js/office.js
+++ frontend/js/office.js
@@ -10,10 +10,10 @@ getOffice((error, officeInfo) => {
       <img src="${ officeInfo[0].logourl}" class="party-pix left">
       <div class="profile-menu">
       <h2 class="title font-serif p-name">
-        <span id="p-name">${officeInfo[0].name}</span>
+        <span id="p-name">${officeInfo[0].name.toUpperCase()}</span>
       </h2>
       <div class="">
-        <strong>Name:</strong> @<span id="p-address">${officeInfo[0].type} </span>
+        <strong>Name:</strong> @<span id="p-address">${officeInfo[0].type.toUpperCase()} </span>
       </div>
     </div>
       `;
@@ -24,10 +24,10 @@ getOffice((error, officeInfo) => {
         officeAll.innerHTML +=       `
             <div class="offices_office" style="height: auto;">
               <h3>
-                <a href="./pages/office.html?id=${office.id}">${office.type}</a>
+                <a href="./pages/office.html?id=${office.id}">${office.name.toUpperCase()}</a>
               </h3>
               <div class="offices_office_options">
-                <span>${office.name}</span>
+                <span>${office.type.toUpperCase()}</span>
               </div>
             </div>`;
       })

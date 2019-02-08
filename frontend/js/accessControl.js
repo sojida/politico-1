fcdbc82e63c6778082c3b4451510d1c1b8622855
Emmanuel Daniel <emmsdan@users.noreply.github.com>
2019-02-08 09:40:51 +0100

    Update accessControl.js

--- frontend/js/accessControl.js
+++ frontend/js/accessControl.js
@@ -43,7 +43,38 @@ async function isLoggedIn() {
     redirect('/access.html?page=signin');
   }
 }
+/**
+ *
+ * @param {string} url
+ */
+const redirect = (url) => {
+  window.location.href = url;
+}
 
+const securePage = () => {
+  const pages = JSON.parse(localStorage.getItem('secure_page'));
+  let secure = false;
+  pages.forEach(page => {
+    if (currentPage().includes(page)) {
+      secure = true;
+      return;
+    }
+  });
+  return secure;
+}
+
+const currentPage = (path = 'full') => {
+  const href = window.location.href;
+  if (path === 'file') {
+    let ihref = window.location.href.split('/');
+    return ihref[ihref.length - 1]
+  }
+  return href;
+}
+
+const throwError = (message, placeholder) => {
+  document.querySelector(placeholder).innerHTML = message;
+}
 /**
  * initialize all the necessary functions
  */
